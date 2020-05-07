4f3ebb49fac7698cf7ff4e4c1c532a83cbec40d1
John Nguyen <jnguyen92780@gmail.com>
2020-05-06 17:11:52 -0700

    entered code block for the App class, including the handleGetGrdsError, handleGetGradesSuccess, getGrades and start methods to app.js

--- components/app.js
+++ components/app.js
@@ -1,2 +1,26 @@
 class App {
-}
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
+  getGrades() {
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      headers: {"X-Access-Token": "2HaPoWNJ"},
+      success: function(response) {
+        console.log(response);
+      },
+      error: this.handleGetGradesError(),
+      })
+    }
+    start() {
+      this.getGrades();
+    }
+  }
