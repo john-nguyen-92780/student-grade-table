761befe2b35d8393c5c470591aab7b88dc489ad4
John Nguyen <jnguyen92780@gmail.com>
2020-05-07 16:31:39 -0700

    in main.js, instantiation of App was modified to include instantiation of GradeForm as an argument

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ var headerElement = document.querySelector("header");
 var newPageHeader = new PageHeader(headerElement);
 var gradeFormElement = document.querySelector("form");
 var newGradeForm = new GradeForm(gradeFormElement);
-var newApp = new App(newGradeTable, newPageHeader);
+var newApp = new App(newGradeTable, newPageHeader, newGradeForm);
 newApp.start();
