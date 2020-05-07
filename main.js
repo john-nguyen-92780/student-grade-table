375f6e2db216cac23badddc03f9591641ef54e61
John Nguyen <jnguyen92780@gmail.com>
2020-05-07 16:30:57 -0700

    in main.js, added a new instantiation of the GradeForm class, passing in the form element as an argument

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ var tableElement = document.querySelector("table");
 var newGradeTable = new GradeTable(tableElement);
 var headerElement = document.querySelector("header");
 var newPageHeader = new PageHeader(headerElement);
+var gradeFormElement = document.querySelector("form");
+var newGradeForm = new GradeForm(gradeFormElement);
 var newApp = new App(newGradeTable, newPageHeader);
 newApp.start();
