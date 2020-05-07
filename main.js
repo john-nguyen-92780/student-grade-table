b5be8bac6c9f012dd06cd33d7ae6c9ec745f5d21
John Nguyen <jnguyen92780@gmail.com>
2020-05-07 12:12:05 -0700

    in main.js, the header element is being apssed in as an argument when the PageHeader is instantiated

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var tableElement = document.querySelector("table");
 var newGradeTable = new GradeTable(tableElement);
+var header = document.querySelector("header");
+var newPageHeader = new PageHeader(header);
 var newApp = new App(newGradeTable);
 newApp.start();
